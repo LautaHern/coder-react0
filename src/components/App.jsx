8465aa0222d8067ecdbcbf547344868a3d83d3f6
Lauta Hern <lautaa.94@gmail.com>
2022-08-30 17:10:57 -0300

    mensajes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,15 @@ import './App.css';
 import React from 'react';
 import Navbar from './Navbar/Navbar';
 import ProdCards from './ProdCards/ProdCards';
+import ItemListContainer from './ItemListContainer/ItemListContainer';
 
 function App() {
   return (
     <>
-    <Navbar></Navbar>
+    <Navbar>
+    </Navbar>
+     <ItemListContainer greeting="Este é um mensagem muito divertido" />
+     <ItemListContainer greeting="This is another funny message" />
     <ProdCards></ProdCards>
     </>
   );
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
-const ItemListContainer = ({prodName, prodPrice, prodDescription} ) => {
+const ItemListContainer = ({prodName, prodPrice, prodDescription, greeting} ) => {
     return (
     <div className="card text-white bg-primary mb-3" style={{maxWidth: '20rem'}}>
+        <div className="greeting">{greeting}</div>
         <div className="card-header">{prodName}</div>
+        
         <div className="card-body">
+            
             <h4 className="card-title">{prodPrice}</h4>
             <p className="card-text">{prodDescription}</p>
         </div> 
+        
     </div>
     );
 }
